2035bc3527c3d00ad7d368b83c6115bbf0245cca
gitpancake <pye.henry@protonmail.com>
2022-08-14 10:32:08 -0400

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const CitizenCropper = () => {
+  const [imageUrl, setImageUrl] = useState<string>("https://crypto-citizens-mainnet.s3.amazonaws.com/4000028.png");
+
+  const draw = () => {
+    var img = new Image();
+    img.src = imageUrl;
+
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+
+    if (!canvas) {
+      console.error("[ERROR] Unable to find canvas element");
+      return;
+    }
+
+    const ctx = canvas.getContext("2d");
+
+    if (!ctx) {
+      console.error("[ERROR] Unable to find canvas context");
+      return;
+    }
+
+    const sourceX = 700;
+    const sourceY = 100;
+    const sourceWidth = 1000;
+    const sourceHeight = 1000;
+
+    // Draw slice
+    ctx.drawImage(img, sourceX, sourceY, sourceWidth, sourceHeight, 0, 0, 600, 600);
+  };
+
+  const handleCitizenChange = () => {
+    setImageUrl(imageUrl);
+    draw();
+  };
+
+  useEffect(() => {
+    draw();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
+      <div style={{ padding: "20px", textAlign: "center" }}>
+        <p>Welcome to Citizen Cropper. Enter the desired citizen image URL below.</p>
+        <p>If you need to get your citizen image URL, you can retrieve it from the artblocks API:</p>
+        <p>{"https://token.artblocks.io/<CONTRACT>/<TOKEN_ID>"}</p>
+        <small>
+          Created by{" "}
+          <a href="https://www.github.com/gitpancake" style={{ textDecoration: "none", color: "grey" }}>
+            gitpancake
+          </a>
+        </small>
+        <br />
+        <small>
+          sauce:
+          <a href="https://www.github.com/gitpancake" style={{ textDecoration: "none", color: "grey" }}>
+            gitpancake
+          </a>
+        </small>
+      </div>
+      <div style={{ padding: "20px" }}>
+        <input type="text" onChange={(e) => setImageUrl(e.target.value)} />
+
+        <button onClick={() => handleCitizenChange()}>Update</button>
+      </div>
+
+      <div>
+        <canvas id="canvas" width="600" height="600"></canvas>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default CitizenCropper;
